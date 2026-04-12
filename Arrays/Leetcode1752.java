7b282ca14b785343b3bb36aed0efefab90fc590b
KathyayiniRangam <kathyayinirangam390@gmail.com>
2026-04-12 19:48:03 +0530

    Leetcode1752

---
+++ Arrays/Leetcode1752.java
@@ -0,0 +1,19 @@
+package Arrays;
+import java.util.Scanner;
+public class Leetcode1752 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[(i+1)%n]){
+                count++;
+            }
+        }
+        System.out.println(count<=1);
+    }
+}
