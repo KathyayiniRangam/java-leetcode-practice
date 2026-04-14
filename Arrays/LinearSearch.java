9a039f300b446e0c6b71ce876602e7029f8f88a1
KathyayiniRangam <kathyayinirangam390@gmail.com>
2026-04-14 19:29:45 +0530

    Linear search

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.Scanner;
+public class LinearSearch {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        boolean found=false;
+        for(int i=0;i<n;i++){
+            if(arr[i]==target){
+                System.out.println("found at index: "+i);
+                found=true;
+            }
+        }
+        if(!found){
+            System.out.println(-1);
+        }
+    }
+}
